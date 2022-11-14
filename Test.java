55bb369e430952a8c27cc6c96b6b985e11d91e89
zx34504 <118162314+zx34504@users.noreply.github.com>
2022-11-14 19:04:37 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,28 @@
+package Air;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("输入机票的原价:");
+        double money = sc.nextDouble();
+
+        System.out.println("请您输入机票的月份:（1-12）");
+        int month = sc.nextInt();
+
+        System.out.println("选择仓位类型:");
+        String type = sc.next();
+
+    }
+
+    public static double Calc(double money,int month,String type){
+        if () {
+
+        } else {
+
+        }
+    }
+}
